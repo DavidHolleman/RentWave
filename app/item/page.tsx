49cbeebe5bc0bc6600ea9252dc9cc07df8e0a475
DavidHolleman <115103613+DavidHolleman@users.noreply.github.com>
2024-11-26 22:38:26 -0600

    start message page

--- app/item/page.tsx
+++ app/item/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
 export default function ItemSearch() {
       return (
-            <p> Search for items </p>
+            <div>
+                  <p> Search Bar </p>
+                  <table>
+                        <tbody>
+                              <tr> <td> <p>Item 1</p> </td> </tr>
+                              <tr> <td> <p>Item 2</p> </td> </tr>
+                              <tr> <td> <p>Item 3</p> </td> </tr>
+                        </tbody>
+                  </table>
+            </div>  
       );
 }
--- app/message/page.tsx
+++ app/message/page.tsx
@@ -1,5 +1,56 @@
+"use client";
+
 export default function Message() {
       return (
-            <p> Messages Page </p>
+            <div className="fillwidth">
+                  <div className="leftcolumn">
+                        <table> <tbody>
+                              <tr className="person">
+                                    <td className="stack"> <p>Item 1</p> </td>
+                                    <td className="stack"> <p>Person 1</p> </td>
+                              </tr>
+                              <tr className="person">
+                                    <td className="stack"> <p>Item 2</p> </td>
+                                    <td className="stack"> <p>Person 2</p> </td>
+                              </tr>
+                              <tr className="person">
+                                    <td className="stack"> <p>Item 3</p> </td>
+                                    <td className="stack"> <p>Person 3</p> </td>
+                              </tr>
+                        </tbody> </table>
+                  </div>
+                  
+                  <div className="rightcolumn">
+                        <table > <tbody>
+                              <tr>
+                                    <div className = "message">
+                                          <td className="stack"> 
+                                                <p className="inline left">Sender 1</p>
+                                                <p className="inline right">Time 1</p>
+                                          </td>
+                                          <td className="stack"> <p>Message 1</p> </td>
+                                    </div>
+                              </tr>
+                              <tr>
+                                    <div className = "message">
+                                          <td className="stack"> 
+                                                <p className="inline left">Sender 2</p>
+                                                <p className="inline right">Time 2</p>
+                                          </td>
+                                          <td className="stack"> <p>Message 2</p> </td>
+                                    </div>
+                              </tr>
+                              <tr>
+                                    <div className = "message">
+                                          <td className="stack"> 
+                                                <p className="inline left">Sender 3</p>
+                                                <p className="inline right">Time 3</p>
+                                          </td>
+                                          <td className="stack"> <p>Message 3</p> </td>
+                                    </div>
+                              </tr>
+                        </tbody> </table>
+                  </div>
+            </div>
       );
 }
