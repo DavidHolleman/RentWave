42f26638aadaf7100836212c097e1a44a5519ffa
XDee0708 <deekafle3@gmail.com>
2024-12-03 13:57:27 -0600

    update review

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,5 +1,13 @@
-export default function Review() {
-      return (
-            <p> Items for you to review, Renters for you to review, and your reviews. </p>
-      );
-}
+
+
+export default function Review() 
+{ return ( 
+<div className="boxone"> 
+      <p> Items for you to review, Renters for you to review, and your reviews. </p> 
+      <textarea rows="5" cols="33"> Please leave a review here... </textarea>
+       </div> ); 
+       
+      
+       
+      
+      }
