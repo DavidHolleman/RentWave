40d7c313d5367a4887d4957b91ac3a7884e893a2
NJL56 <nlachcik0506@gmail.com>
2024-12-05 09:40:12 -0600

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 "use client";
-
+import { Input } from "@/app/components/Input";
+import { useFormState, useFormStatus } from "react-dom";
 import Image from "next/image";
 
+function SubmitButton() {
+	const { pending } = useFormStatus();
+	return (
+		<input
+			className="mb-0"
+			type="submit"
+			value={pending ? "Searching" : "Search"}
+		/>
+	);
+}
+
+function trySearch(state: string, payload: FormData): string | Promise<string> {
+  throw new Error("Function not implemented.");
+}
+
 export default function Home() {
+  let [error, formAction] = useFormState<string, FormData>(trySearch, "");
   return (
     <div style={{ height: "90vh", display: "flex", flexDirection:"column", alignItems:"center", justifyContent:"center", gap:20}}>
+      <h2>RentWave</h2>
       <Image
         src="/logo.png"
         alt="rentwave logo"
@@ -12,7 +30,30 @@ export default function Home() {
         height={180}
         priority
       />
-      <h2> Welcome to RentWave! </h2>
+      <h2>A Global Renting Solution</h2>
+      <div className="box">
+            
+            
+            <div className="form">
+                <form action={formAction}>
+                    <Input
+                        label=""
+                        type="text"
+                        id="search"
+                        name="search"
+                        required={true}
+                        placeholder="Search"
+                    />
+                    
+                    <SubmitButton />
+                    <p> {error} </p>
+                </form>
+            </div>
+        </div>
+      <h2>Item 1</h2>
+      <h2>Item 2</h2>
+      <h2>Item 3</h2>
     </div>
+    
   );
 }
